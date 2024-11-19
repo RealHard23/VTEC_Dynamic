30259c4006126f3259d593341e429aa35126d42b
RealHard <155993628+RealHard23@users.noreply.github.com>
2024-11-20 00:40:33 +0700

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -56,7 +56,7 @@ if [ -e /sys/class/thermal/thermal_message/sconfig ]; then
 su -c settings put global hwui.disable_vsync false
 su -c settings put global touch_response_time 0
 su -c settings put global foreground_ram_priority high
-u -c cmd power set-fixed-performance-mode-enabled true
+su -c cmd power set-fixed-performance-mode-enabled true
 #su -c cmd thermalservice override-status 0
 su -c settings put system power_mode high
 su -c settings put system speed_mode 1
@@ -88,5 +88,81 @@ nohup sh $MODDIR/script/shellscript > /dev/null &
 sync && echo 3 > /proc/sys/vm/drop_caches
 echo "Optimizations applied successfully!"
 
-# Disable other unnecessary debugging options as per your needs
-exit 0
+#!/data/adb/magisk/busybox sh
+#CREDIT
+#Bootloop saver by HuskyDG, modified by ez-me
+
+# Get variables
+MODPATH=${0%/*}
+MESSAGE="$(cat "$MODPATH"/msg.txt | head -c100)"
+
+# Log
+log(){
+   TEXT=$@; echo "[`date -Is`]: $TEXT" >> $MODPATH/log.txt
+}
+
+log "Started"
+
+# Modify description
+cp "$MODPATH/module.prop" "$MODPATH/temp.prop"
+sed -Ei "s/^description=(\[.*][[:space:]]*)?/description=[Working🥳. $MESSAGE] /g" "$MODPATH/temp.prop"
+mv "$MODPATH/temp.prop" "$MODPATH/module.prop"
+
+# Define the function
+disable_modules(){
+   log "Disabling modules..."
+   list="$(find /data/adb/modules/* -prune -type d)"
+   for module in $list
+   do
+      touch $module/disable
+   done
+   rm -rf "$MODPATH/disable"
+   echo "Disabled modules at $(date -Is)" > "$MODPATH/msg.txt"
+   rm -rf /cache/.system_booting /data/unencrypted/.system_booting /metadata/.system_booting /persist/.system_booting /mnt/vendor/persist/.system_booting
+   log "Rebooting"
+   log ""
+   reboot
+   exit
+}
+
+
+# Gather PIDs
+sleep 5
+ZYGOTE_PID1=$(getprop init.svc_debug_pid.zygote)
+log "PID1: $ZYGOTE_PID1"
+
+sleep 15
+ZYGOTE_PID2=$(getprop init.svc_debug_pid.zygote)
+log "PID2: $ZYGOTE_PID2"
+
+sleep 15
+ZYGOTE_PID3=$(getprop init.svc_debug_pid.zygote)
+log "PID3: $ZYGOTE_PID3"
+
+# Check for BootLoop
+log "Checking..."
+
+if [ -z "$ZYGOTE_PID1" ]
+then
+   log "Zygote didn't start?"
+   disable_modules
+fi
+
+if [ "$ZYGOTE_PID1" != "$ZYGOTE_PID2" -o "$ZYGOTE_PID2" != "$ZYGOTE_PID3" ]
+then
+   log "PID mismatch, checking again"
+   sleep 15
+   ZYGOTE_PID4=$(getprop init.svc_debug_pid.zygote)
+   log "PID4: $ZYGOTE_PID4"
+
+   if [ "$ZYGOTE_PID3" != "$ZYGOTE_PID4" ]
+   then
+      log "They don't match..."
+      disable_modules
+   fi
+fi
+
+# If  we reached this section we should be fine
+log "looks good to me!"
+log ""
+exit
